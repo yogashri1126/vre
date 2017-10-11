f2af768130d39d54cf1034d82fd352b848082e54
yogashri1126 <ypradhan.pge@texas.edu>
2017-10-10 21:44:35 -0500

    Added two objects for volunteer and rescuee. I dont know if you would want different variable objects or just one object, but make a true/false if its a volunteer or a rescuee

--- data/data.js
+++ data/data.js
@@ -3,7 +3,8 @@
 //put jQuery here
 //make ajax request $post here
 
-// var person = [name: ($name).val.trim(), 
+// var volunteer= [
+//name: ($name).val.trim(), 
 //address: ($address).val.trim(), 
 //January: ($January).val.trim(),
 //February: ($February).val.trim(),
@@ -18,4 +19,22 @@
 //November: ($November).val.trim(),
 //December: ($December).val.trim(),
 //Truck: ($Truck).val.trim(),
-//etc etc it depends on the materials y'all asked of them and the occupation y'all ask of them];
+//etc etc it depends on the materials y'all asked of them and the occupation y'all ask of them]
+
+// var rescuee= [
+//name: ($name).val.trim(), 
+//address: ($address).val.trim(), 
+//January: ($January).val.trim(),
+//February: ($February).val.trim(),
+//March: ($March).val.trim(),
+//April: ($April).val.trim(),
+//May: ($May).val.trim(),
+//June: ($June).val.trim(),
+//July: ($July).val.trim(),
+//August: ($August).val.trim(),
+//September: ($September).val.trim(),
+//October: ($October).val.trim(),
+//November: ($November).val.trim(),
+//December: ($December).val.trim(),
+//Truck: ($Truck).val.trim(),
+//etc etc it depends on the materials y'all asked of them and the occupation y'all ask of them]
