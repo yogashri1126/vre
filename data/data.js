36aabbe5b71f4f9661d16299d0a2f9c89c2aed91
yogashri1126 <ypradhan.pge@texas.edu>
2017-10-10 21:42:16 -0500

    put example jQuery array within object for Leah and Dan

--- data/data.js
+++ data/data.js
@@ -2,3 +2,20 @@
 
 //put jQuery here
 //make ajax request $post here
+
+// var person = [name: ($name).val.trim(), 
+//address: ($address).val.trim(), 
+//January: ($January).val.trim(),
+//February: ($February).val.trim(),
+//March: ($March).val.trim(),
+//April: ($April).val.trim(),
+//May: ($May).val.trim(),
+//June: ($June).val.trim(),
+//July: ($July).val.trim(),
+//August: ($August).val.trim(),
+//September: ($September).val.trim(),
+//October: ($October).val.trim(),
+//November: ($November).val.trim(),
+//December: ($December).val.trim(),
+//Truck: ($Truck).val.trim(),
+//etc etc it depends on the materials y'all asked of them and the occupation y'all ask of them];
