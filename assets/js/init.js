f64f0e4998884430c9e4f50ce300512a04767de2
LeahStep77 <fanaticalbotanical@yahoo.com>
2017-10-10 19:49:57 -0500

    updating file structure

--- assets/js/init.js
+++
@@ -1,8 +0,0 @@
-
-
-$(document).ready(function() {
-    $('#modal1').modal();
-    $('#modal2').modal();
-    $('#modal3').modal();
-    $('#modal4').modal();
-});
---
+++ public/assets/js/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+    $('#modal1').modal();
+    $('#modal2').modal();
+    $('#modal3').modal();
+    $('#modal4').modal();
+});
