d2ebe10b009baed206e039c2dfc95608952fb12e
yogashri1126 <ypradhan.pge@texas.edu>
2017-10-14 10:16:49 -0500

    solved the forward slash issues in config file

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/..\config\config.json')[env];
+var config    = require(__dirname + "/../config/config.json")[env];
 var db        = {};
 
 if (config.use_env_variable) {
@@ -21,6 +21,7 @@ fs
   })
   .forEach(file => {
     var model = sequelize['import'](path.join(__dirname, file));
+    console.log(model);
     db[model.name] = model;
   });
 
--- server.js
+++ server.js
@@ -6,6 +6,9 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var path= require("path");
+var urlRelative= require("url-relative");
+
 
 // Sets up the Express App
 // =============================================================
@@ -13,8 +16,8 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require("./models");
-
+var db = require(path.join( __dirname,"/models"));
+//var correctedPath= "models/"
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
